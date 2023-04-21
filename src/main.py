54ee08877002eb41c70a99c1477fd5b08800cc3b
Nrittik Sarmah <nrittik43@gmail.com>
2023-04-21 22:33:03 +0530

    dataset integration

--- src/main.py
+++ src/main.py
@@ -2,8 +2,13 @@
 import make_graph
 import community
 import metric
+import os
+import networkx as nx
+import os
 
-def evaluate_network(dataset):
+cur_path = os.path.dirname(__file__)
+
+def evaluate_network(dataset:str):
     for i in range(1, 5):
         path = dataset + str(i) + ".csv"
         G = make_graph.convert_input_to_graph(path)
@@ -15,4 +20,23 @@ def evaluate_network(dataset):
             communities[value].append(key)
         print(metric.modularity_metric(G, part), metric.conductance_metric(G, communities), metric.fscore_metric(G,part))
 
-print(evaluate_network("../dataset/15node/15node_t0"))
+# print(evaluate_network("../dataset/15node/15node_t0"))
+
+inputFile = 'input.txt'
+outputFile = 'output.txt'
+def execFile(filename):
+    file = open(os.path.join(filename, 'input.txt'),'r')
+    data = file.read()
+    with open(inputFile, 'w+') as filetowrite:
+        filetowrite.write(data)
+    os.system('python algo.py')
+    with open(outputFile, 'r') as filetoread:
+        data = [[int(num) for num in line.split()] for line in filetoread]
+    with open(os.path.join(filename, 't1.txt'), 'r') as gFile:
+        edgelist = [[int(num) for num in line.split()] for line in gFile]
+        G = nx.from_edgelist(edgelist)
+        print(G)
+
+new_path = os.path.join(cur_path, 'dataset', 'demo')
+execFile(new_path)
+    
