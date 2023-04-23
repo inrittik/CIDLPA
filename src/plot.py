581c5133f636f56f44f08505d8a08efb117de37c
talukdarudit <78261320+talukdarudit@users.noreply.github.com>
2023-04-23 12:15:04 +0530

    Update plot.py

--- src/plot.py
+++ src/plot.py
@@ -1,58 +1,15 @@
 # Implmentation of the generalized plot function
-# Implmentation of the generalized plot function
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 
-def plot_comm(n, path_input, path_result, label):
+def plot_comm( communities_list: list[list[int]], graph: nx.Graph()):
 
-    # reading file given by path
-    f = open(path_result, 'r')
-    Lines = f.readlines()
-    
-    # storing the communities from file to the dictionary `communities`
-    print("Communities: ")
-    comm_color = {}
-    communities = {}
-    
-    i=0
-    for line in Lines:
-        a = list(map(int,line.split()))
-        print(a)
-        communities[i] = a
-        i += 1
+    communities={}
+    for i, community in enumerate(communities_list):
+        communities[i] = community
 
-    count1 = 0
-    t=0
-    A = []
-    for i in range(0, n):
-        A.append(list(np.zeros(n, dtype=int)))
 
-    f1 = open(path_input, 'r')
-    Lines1 = f1.readlines()
-    kk = 0
-    for line in Lines1:
-        if kk == 0:
-            kk = 1
-            continue
-        a = list(map(int,line.split()))
-        if len(a) == 2:
-            A[a[0]][a[1]] = 1
-    
-    f1.close()     
-    f.close()
-
-    # graph will be returned and later used in analysis part 
-    graph = nx.Graph()
-    for i in range(0,n):
-        graph.add_node(i)
-
-    for i in range(0,n):
-        for j in range(0,n):
-            if A[i][j] == 1:
-                graph.add_edge(i,j)
-                graph.add_edge(j,i)
-    
     # set of distinct colors
     node_color = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff'] 
     fig, ax = plt.subplots(figsize=(12, 8))
@@ -61,7 +18,7 @@ def plot_comm(n, path_input, path_result, label):
     n = graph.number_of_nodes()
     color = ['#ffffff' for i in range(n) ]
     
-    # assignig each node color of it's community
+    # assigning each node color of it's community
     t=0
     vis = {}
     for com in range(len(communities)) :
@@ -91,14 +48,3 @@ def plot_comm(n, path_input, path_result, label):
     plt.show()
     
     return graph
-
-from igraph import *
-
-# Create a random graph with 30 nodes and 0.3 edge probability
-g = Graph.Erdos_Renyi(30, 0.3)
-
-# Find the communities using community_multilevel method
-comms = g.community_multilevel()
-
-# Plot the graph with the communities marked by different colors
-plot(comms, mark_groups=True)
